8e084b5b3a07b3c3b746b9921cf0272273f170b7
Nathan Perkins <nathan@maxmo.net>
2015-06-28 10:09:12 -0400

    Initial Commit

---
+++ VideoCapture/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  VideoCapture
+//
+//  Created by Nathan Perkins on 6/28/15.
+//  Copyright (c) 2015 GardnerLab. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
