1a8b72ada477721b3d94d779decb4b01335a495f
Nathan Perkins <lnp@bu.edu>
2016-02-10 11:06:33 -0500

    Add preferences view controller

---
+++ VideoCapture/PreferencesViewController.swift
@@ -0,0 +1,18 @@
+//
+//  PreferencesViewController.swift
+//  VideoCapture
+//
+//  Created by Nathan Perkins on 2/10/16.
+//  Copyright © 2016 GardnerLab. All rights reserved.
+//
+
+import Cocoa
+
+class PreferencesViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
